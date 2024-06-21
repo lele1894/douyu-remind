bccb6faa7db323ec2f0a6598b093f6977c5f7d9c
lele1894 <lele1894@163.com>
2024-06-21 13:22:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,27 +1,22 @@
 import os
 import subprocess
-import shutil
 
-CRD_SSH_Code = input("谷歌CRD SSH代码 :")
-username = "user" #@param {type:"string"}
-password = "root" #@param {type:"string"}
+CRD_SSH_Code = input("Google CRD SSH Code :")
+username = "user"  # @param {type:"string"}
+password = "root"  # @param {type:"string"}
 os.system(f"useradd -m {username}")
 os.system(f"adduser {username} sudo")
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
 os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
 
-Pin = 123456 #@param {type: "integer"}
-Autostart = True #@param {type: "boolean"}
+Pin = 123456  # @param {type: "integer"}
 
 class CRDSetup:
     def __init__(self, user):
         os.system("apt update")
         self.installCRD()
         self.installDesktopEnvironment()
-        self.changewall()
         self.installGoogleChrome()
-        self.installTelegram()
-        self.installQbit()
         self.finish(user)
 
     @staticmethod
@@ -29,7 +24,7 @@ class CRDSetup:
         subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'])
         subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'])
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Chrome 远程桌面已安装 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("Chrome Remote Desktop 已安装 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
     @staticmethod
     def installDesktopEnvironment():
@@ -39,64 +34,24 @@ class CRDSetup:
         os.system("apt remove --assume-yes gnome-terminal")
         os.system("apt install --assume-yes xscreensaver")
         os.system("systemctl disable lightdm.service")
-        print("安装的XFCE4桌面环境 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("已安装 XFCE4 桌面环境 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
     @staticmethod
     def installGoogleChrome():
         subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"])
         subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"])
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("已安装谷歌浏览器 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-      
-    """
-    @staticmethod
-    def installTelegram():
-        subprocess.run(["apt", "install", "--assume-yes", "telegram-desktop"])
-        print("已安装电报 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    
-    @staticmethod
-    def changewall():
-        os.system(f"curl -s -L -k -o xfce-verticals.png https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg")
-        current_directory = os.getcwd()
-        custom_wallpaper_path = os.path.join(current_directory, "xfce-verticals.png")
-        destination_path = '/usr/share/backgrounds/xfce/'
-        shutil.copy(custom_wallpaper_path, destination_path)
-        print("壁纸已更改 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    
-    
-    @staticmethod
-    def installQbit():
-        subprocess.run(["sudo", "apt", "update"])
-        subprocess.run(["sudo", "apt", "install", "-y", "qbittorrent"])
-        print("已安装 Qbittorrent !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    """
+        print("Google Chrome 已安装 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
     @staticmethod
     def finish(user):
-        if Autostart:
-            os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
-            link = "www.youtube.com/@The_Disala"
-            colab_autostart = """[Desktop Entry]
-            print("完成 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-Type=Application
-Name=Colab
-Exec=sh -c "sensible-browser {}"
-Icon=
-Comment=Open a predefined notebook at session signin.
-X-GNOME-Autostart-enabled=true""".format(link)
-            with open(f"/home/{user}/.config/autostart/colab.desktop", "w") as f:
-                f.write(colab_autostart)
-            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
-            os.system(f"chown {user}:{user} /home/{user}/.config")
-            
         os.system(f"adduser {user} chrome-remote-desktop")
         command = f"{CRD_SSH_Code} --pin={Pin}"
         os.system(f"su - {user} -c '{command}'")
         os.system("service chrome-remote-desktop start")
         
         print("..........................................................") 
-        print(".....Brought By The Disala................................") 
+        print(".....由 The Disala 提供................................") 
         print("..........................................................") 
         print("......#####...######...####....####...##.......####.......") 
         print("......##..##....##....##......##..##..##......##..##......")  
@@ -104,9 +59,7 @@ X-GNOME-Autostart-enabled=true""".format(link)
         print("......##..##....##........##..##..##..##......##..##......") 
         print("......#####...######...####...##..##..######..##..##......") 
         print("..........................................................") 
-        print("..YouTube 视频教程 - https://youtu.be/xqpCQCJXKxU ..") 
-        print("..........................................................") 
-        print("登录 PIN 码 : 123456") 
+        print("登录 PIN : 123456") 
         print("用户名 : user") 
         print("用户密码 : root") 
         while True:
@@ -114,10 +67,10 @@ X-GNOME-Autostart-enabled=true""".format(link)
 
 try:
     if CRD_SSH_Code == "":
-        print("请输入https://remotedesktop.google.com/headless授权码")
+        print("请输入从链接获取的授权码")
     elif len(str(Pin)) < 6:
-        print("输入大于或等于 6 位数字的 PIN")
+        print("请输入6位或以上的PIN码")
     else:
         CRDSetup(username)
 except NameError as e:
-    print("'未找到 username' 变量，请先创建用户")
+    print("'username' 变量未找到，请先创建用户")
