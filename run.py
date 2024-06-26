e95e8210f863300b4ca669529784435e09c22545
lele1894 <lele1894@163.com>
2024-06-26 21:32:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,76 +1,17 @@
-import os
-import subprocess
+import requests
 
-CRD_SSH_Code = input("Google CRD SSH Code :")
-username = "user"  # @param {type:"string"}
-password = "root"  # @param {type:"string"}
-os.system(f"useradd -m {username}")
-os.system(f"adduser {username} sudo")
-os.system(f"echo '{username}:{password}' | sudo chpasswd")
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
-
-Pin = 123456  # @param {type: "integer"}
-
-class CRDSetup:
-    def __init__(self, user):
-        os.system("apt update")
-        self.installCRD()
-        self.installDesktopEnvironment()
-        self.installGoogleChrome()
-        self.finish(user)
-
-    @staticmethod
-    def installCRD():
-        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'])
-        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'])
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Chrome Remote Desktop 已安装 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-    @staticmethod
-    def installDesktopEnvironment():
-        os.system("export DEBIAN_FRONTEND=noninteractive")
-        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
-        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
-        os.system("apt remove --assume-yes gnome-terminal")
-        os.system("apt install --assume-yes xscreensaver")
-        os.system("systemctl disable lightdm.service")
-        print("已安装 XFCE4 桌面环境 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-    @staticmethod
-    def installGoogleChrome():
-        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"])
-        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"])
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Google Chrome 已安装 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-    @staticmethod
-    def finish(user):
-        os.system(f"adduser {user} chrome-remote-desktop")
-        command = f"{CRD_SSH_Code} --pin={Pin}"
-        os.system(f"su - {user} -c '{command}'")
-        os.system("service chrome-remote-desktop start")
-        
-        print("..........................................................") 
-        print(".....由 The Disala 提供................................") 
-        print("..........................................................") 
-        print("......#####...######...####....####...##.......####.......") 
-        print("......##..##....##....##......##..##..##......##..##......")  
-        print("......##..##....##.....####...######..##......######......") 
-        print("......##..##....##........##..##..##..##......##..##......") 
-        print("......#####...######...####...##..##..######..##..##......") 
-        print("..........................................................") 
-        print("登录 PIN : 123456") 
-        print("用户名 : user") 
-        print("用户密码 : root") 
-        while True:
-            pass
-
-try:
-    if CRD_SSH_Code == "":
-        print("请输入从链接获取的授权码")
-    elif len(str(Pin)) < 6:
-        print("请输入6位或以上的PIN码")
+def check_douyu(room_id):
+    url = f"https://open.douyucdn.cn/api/RoomApi/room/{room_id}"
+    response = requests.get(url)
+    data = response.json()
+    
+    if data['data']['room_status'] == '1':
+        print("斗鱼正常")
     else:
-        CRDSetup(username)
-except NameError as e:
-    print("'username' 变量未找到，请先创建用户")
+        print("斗鱼关播")
+        requests.post("http://miaotixing.com/trigger?id=tzLyHCO", data={"text": "斗鱼关播/"})
+
+if __name__ == "__main__":
+    import sys
+    room_id = sys.argv[1] if len(sys.argv) > 1 else "292098"
+    check_douyu(room_id)
